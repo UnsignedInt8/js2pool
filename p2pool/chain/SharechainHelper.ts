18609cca6fd5bad1ae867da4f0b0342d5b056ce6
UnsignedInt8 <unsignedint8@gmail.com>
2017-05-14 08:51:18 +0800

    Fix folders checking

--- p2pool/chain/SharechainHelper.ts
+++ p2pool/chain/SharechainHelper.ts
@@ -34,14 +34,9 @@ export class SharechainHelper {
         SharechainHelper.appDir = appDir;
         SharechainHelper.dataDir = dataDir;
 
-        if (!fs.existsSync(appDir) || !fs.existsSync(path.resolve(appDir, 'data'))) {
-            fs.mkdirSync(appDir);
-            fs.mkdirSync(path.resolve(appDir, 'data'));
-        }
-
-        if (!fs.existsSync(dataDir)) {
-            fs.mkdirSync(dataDir);
-        }
+        if (!fs.existsSync(appDir)) fs.mkdirSync(appDir);
+        if (!fs.existsSync(path.resolve(appDir, 'data'))) fs.mkdirSync(path.resolve(appDir, 'data'));
+        if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir);
 
         let deprecatedFiles = fs.readdirSync(dataDir).where(name => Number.parseInt(name.split('_')[1]) < SharechainHelper.today - 2).select(name => path.resolve(dataDir, name)).toArray();
         deprecatedFiles.each(path => fs.unlinkSync(path));
