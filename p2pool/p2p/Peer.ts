5572449cc864382ca6262be782dd9c4bd5749f3e
UnsignedInt8 <unsignedint8@gmail.com>
2017-05-18 11:04:24 +0800

    cancel extra 1 item

--- p2pool/p2p/Peer.ts
+++ p2pool/p2p/Peer.ts
@@ -209,7 +209,7 @@ export class Peer {
     }
 
     private handleSharereq(sender: Node, request: TypeSharereq) {
-        let parents = Math.min(Math.min(request.parents, 1000 / request.hashes.length | 0), sender.isJs2PoolPeer ? 250 : 100) + 1;
+        let parents = Math.min(Math.min(request.parents, 1000 / request.hashes.length | 0), sender.isJs2PoolPeer ? 250 : 100);
         let stops = new Set(request.stops);
         let shares = new Array<BaseShare>();
 
